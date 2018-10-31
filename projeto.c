81b705f95e1e5b6a8d2af21ded138ce6cb12fd52
Arthur <arthursmr17@gmail.com>
2018-10-31 20:56:22 -0300

    cadastro aluno

--- projeto.c
+++ projeto.c
@@ -4,7 +4,7 @@
 
 typedef struct Aluno{
     char nome[100];
-    long int ra;
+    int ra;
     char login[30];
     char senha[30];
 }Aluno;
@@ -20,7 +20,7 @@ int validaLogin(char usuario[30], char senha[30], Disciplina *d){
     char usuarioRes[30], senhaRes[30], nome[30];
     FILE* fp;
     
-    if ((fp = fopen("./cadastros/Alunos.txt", "r")) == NULL){
+    if ((fp = fopen("Alunos.txt", "r")) == NULL){
         printf("Nenhum aluno previamente cadastrado\n");        
     }    
 
@@ -36,17 +36,83 @@ int validaLogin(char usuario[30], char senha[30], Disciplina *d){
         fscanf(fp, "%[^\n]", senhaRes);            
         
         if(strcmp(usuario, usuarioRes) == 0 && strcmp(senha, senhaRes) == 0){                
+
             loginRes = 1;
             break;
         }
         cont++;
-    }        
+    }
+      
     fclose(fp);
     return loginRes;
 }
 
+Aluno * newAluno(char *nome, int ra, char *login, char *senha){
+    Aluno * aux = (Aluno*)malloc(sizeof(Aluno));
+    strcpy(aux->nome, nome);
+    aux->ra = ra;
+    strcpy(aux->login, login);
+    strcpy(aux->senha, senha);
+    return aux;
+}
+Aluno * le(){
+    //le um aluno do teclado e faz um return.
+    char nome[100];
+    int ra;
+    char login[30];
+    char senha[30];
+    printf("Digite o seu nome: ");
+    gets(nome);
+    printf("Digite seu RA: ");
+    scanf("%d", &ra);
+    getchar();
+    printf("Digite seu Login: ");
+    gets(login);
+    printf("Digite sua senha: ");
+    gets(senha);
+    return newAluno(nome,ra,login,senha);
+}
+
+
+void salvaAluno(Disciplina * d){
+    FILE * f;
+    f = fopen("Alunos.txt","w");
+    fprintf(f,"%d\n",d->top);
+    for(int aluno=0;aluno<d->top;aluno++){
+        fprintf(f,"%d,",d->v[aluno]->ra);
+        fprintf(f,"%s,",d->v[aluno]->nome);
+        fprintf(f,"%s,",d->v[aluno]->login);
+        fprintf(f,"%s\n",d->v[aluno]->senha);
+    }
+    fclose(f);
+}
+
+Disciplina * carregaD()
+{
+//le o que tem no .txt
+    FILE * f;
+    Disciplina * d;
+    int cont=0;
+    d = (Disciplina*)malloc(sizeof(Disciplina));
+    f = fopen("Alunos.txt","r"); //ler o que tem no dados.txt
+    fscanf(f, "%d", &d->top);
+    while(cont < d->top){
+        d->v[cont] = newAluno("a", 0, "a", "a");
+        fscanf(f, "%d", &d->v[cont]->ra);
+        fseek(f, +1, SEEK_CUR);
+        fscanf(f, "%[^,]", d->v[cont]->nome);
+        fseek(f, +1, SEEK_CUR);
+        fscanf(f, "%[^,]", d->v[cont]->login);
+        fseek(f, +1, SEEK_CUR);
+        fscanf(f, "%[^\n]", d->v[cont]->senha); 
+        cont++;
+    }
+    fclose(f); 
+    return d;
+}
+
 int main(){        
-    int loginRes = 0;
+    int loginRes = 0, opcao=-1;
     char usuario[30], senha[30];
     Disciplina *d = (Disciplina *)malloc(sizeof(Disciplina));
 
@@ -59,10 +125,27 @@ int main(){
 
         loginRes = validaLogin(usuario, senha, d);
         if(loginRes == 0){
-            printf("Usuario ou senha incorreta\n");            
+            printf("Usuario ou senha incorreta\n\n");            
         }else{
-            printf("Login efetuado\n");
+            printf("Login efetuado\n\n");
         }        
+    }
+
+    d = carregaD();
+
+    while(opcao != 0){
+        printf("1->Cadastro aluno\n");
+        printf("Digite a opcao:");
+        scanf("%d",&opcao);
+        getchar();
+        switch(opcao){
+            case 0: break;
+            case 1:
+                d->v[d->top++]=le();
+                salvaAluno(d);
+                printf("Cadastro realizado com sucesso!\n");
+                break;
+        }
     }
     
     return 0;
